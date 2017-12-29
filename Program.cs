3738aae0d88df21031a60ddedd9877b66d43e1d2
PoroCYon <pcy@national.shitposting.agency>
2017-12-29 16:56:27 +0100

    Add XDG protocol registration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Conduit
@@ -22,12 +24,54 @@ namespace Conduit
 
       subkey.CreateSubKey("shell").CreateSubKey("open").CreateSubKey("command").SetValue(null, "\"" + exePath + "\" -openURL %1");
     }
+    private static void RegisterProtocolXDG(string exePath)
+    {
+      const string name = "Conduit";
+
+      string desktopFile =
+"[Desktop Entry]\n" +
+"Type=Application\n" +
+"Version=1.0\n" +
+"Name=" + name + "\n" +
+"Exec=mono " + exePath + " -openURL\n" +
+"Terminal=false\n";
+
+      string dir = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
+      if (String.IsNullOrEmpty(dir))
+      {
+        dir = Environment.GetEnvironmentVariable("HOME");
+        if (String.IsNullOrEmpty(dir))
+        {
+          dir = "/home/" + Environment.GetEnvironmentVariable("USER"); // good enough
+        }
+        dir += "/.local/share";
+      }
+
+      dir += "/applications";
+
+      if (!Directory.Exists(dir))
+      {
+        Directory.CreateDirectory(dir);
+      }
+
+      string file = dir + "/conduit.desktop";
+
+      File.WriteAllText(file, desktopFile);
+
+      // $ xdg-settings set default-url-scheme-handler conduit conduit.desktop
+      var psi = new ProcessStartInfo("xdg-settings", "set default-url-scheme-handler conduit conduit.desktop");
+      Process.Start(psi).WaitForExit();
+    }
 
     private static void RegisterProtocol()
     {
       if (Environment.OSVersion.Platform.HasFlag(PlatformID.Win32NT))
       {
         RegisterProtocolWindows(System.Reflection.Assembly.GetEntryAssembly().Location);
+      }
+      else if (Environment.OSVersion.Platform.HasFlag(PlatformID.Unix))
+      {
+        RegisterProtocolXDG(System.Reflection.Assembly.GetEntryAssembly().Location);
       }
     }
 
