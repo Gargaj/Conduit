956d9e79d6d42dc60620a547ebe370ed787981a7
Gargaj <gargaj@conspiracy.hu>
2018-02-08 13:40:10 +0100

    Don't display downloader if download link is missing (demozoo)

--- WebExtension/demozoo.js
+++ WebExtension/demozoo.js
@@ -1,13 +1,17 @@
-var re = /productions\/(\d+)/ig;
-var prodID = re.exec(location.href);
+var download_links = document.getElementsByClassName("download_links");
+if(download_links && download_links.length)
+{
+  var re = /productions\/(\d+)/ig;
+  var prodID = re.exec(location.href);
 
-var right = document.getElementsByClassName("right")[0];
+  var right = document.getElementsByClassName("right")[0];
 
-var block = document.createElement("div");
-block.id = "conduit";
-block.innerHTML = 
-  "<h3>Download and run</h3>\n"+
-  "<div><a href='conduit://demozoo/prod/"+parseInt(prodID[1],10)+"'><b>Click here</b></a> to download and watch this prod immediately with <a href='"+CONDUIT_URL+"'>Conduit</a>"
-block.className = "panel";
+  var block = document.createElement("div");
+  block.id = "conduit";
+  block.innerHTML = 
+    "<h3>Download and run</h3>\n"+
+    "<div><a href='conduit://demozoo/prod/"+parseInt(prodID[1],10)+"'><b>Click here</b></a> to download and watch this prod immediately with <a href='"+CONDUIT_URL+"'>Conduit</a>"
+  block.className = "panel";
 
-right.insertBefore(block,right.childNodes[0]);
+  right.insertBefore(block,right.childNodes[0]);
+}
